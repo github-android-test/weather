9c6912c9bacc8036f90ac8ee44e9b2a9a978d86c
Bojan <fokabezobaoka@gmail.com>
2019-11-27 05:50:07 +0100

    implemented basic navigation via bottom navigation menu and view pager

--- app/src/main/java/com/bojanpavlovic/weather/view/MainActivity.java
+++ app/src/main/java/com/bojanpavlovic/weather/view/MainActivity.java
@@ -1,13 +1,18 @@
 package com.bojanpavlovic.weather.view;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 
 import com.bojanpavlovic.weather.R;
 import com.bojanpavlovic.weather.interfaces.IWeatherService;
 import com.bojanpavlovic.weather.model.CurrentWeatherModel;
 import com.bojanpavlovic.weather.model.ForecastWeatherModel;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -17,10 +22,22 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
+    ViewPager viewPager;
+    BottomNavigationView bottomNavigationView;
+    CurrentWeatherFragment currentWeatherFragment;
+    ForecastWeatherFragment forecastWeatherFragment;
+    SettingsFragment settingsFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewPager = findViewById(R.id.view_pager);
+        bottomNavigationView = findViewById(R.id.bottom_navigation_view);
+        setupViewPager(viewPager);
+        setBottomNavigationBehaviour(bottomNavigationView, viewPager);
+        setViewPagerBehaviour(bottomNavigationView, viewPager);
 
         //================================
         // TODO Remove later, just for test
@@ -62,10 +79,82 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-
-
-
-
-
     }
+
+    private void setupViewPager(ViewPager viewPager){
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        currentWeatherFragment = new CurrentWeatherFragment();
+        forecastWeatherFragment = new ForecastWeatherFragment();
+        settingsFragment = new SettingsFragment();
+        viewPagerAdapter.addFragment(currentWeatherFragment);
+        viewPagerAdapter.addFragment(forecastWeatherFragment);
+        viewPagerAdapter.addFragment(settingsFragment);
+        viewPager.setAdapter(viewPagerAdapter);
+    }
+
+    private void setBottomNavigationBehaviour(BottomNavigationView bottomNavigationView, final ViewPager viewPager){
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.bottom_navigation_item_weather:
+                        Log.i("WEATHER_LOG", "onNavigationItemSelected 0");
+                        viewPager.setCurrentItem(0);
+                        return true;
+                    case R.id.bottom_navigation_item_forecast:
+                        Log.i("WEATHER_LOG", "onNavigationItemSelected 1");
+                        viewPager.setCurrentItem(1);
+                        return true;
+                    case R.id.bottom_navigation_item_settings:
+                        Log.i("WEATHER_LOG", "onNavigationItemSelected 2");
+                        viewPager.setCurrentItem(2);
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    private void setViewPagerBehaviour(final BottomNavigationView bottomNavigationView, ViewPager viewPager){
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                bottomNavigationView.getMenu().getItem(0).setChecked(false);
+                bottomNavigationView.getMenu().getItem(1).setChecked(false);
+                bottomNavigationView.getMenu().getItem(2).setChecked(false);
+                switch (position){
+                    // Todo Implement function to uncheck all menu items first
+                    case 0:
+                        Log.i("WEATHER_LOG", "onPageSelected 0");
+                        bottomNavigationView.getMenu().getItem(0).setChecked(true);
+                        break;
+                    case 1:
+                        Log.i("WEATHER_LOG", "onPageSelected 1");
+                        bottomNavigationView.getMenu().getItem(1).setChecked(true);
+                        break;
+                    case 2:
+                        Log.i("WEATHER_LOG", "onPageSelected 2");
+                        bottomNavigationView.getMenu().getItem(2).setChecked(true);
+                        break;
+                    default:
+                        Log.i("WEATHER_LOG", "onPageSelected DEFAULT");
+
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+    }
+
+
+
+
 }
---
+++ app/src/main/java/com/bojanpavlovic/weather/view/ViewPagerAdapter.java
@@ -0,0 +1,31 @@
+package com.bojanpavlovic.weather.view;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+    private final List<Fragment> mFragmentList = new ArrayList<>();
+
+    public ViewPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return position > 0  ? mFragmentList.get(position) : mFragmentList.get(0);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragmentList.size();
+    }
+
+    public void addFragment(Fragment fragment){
+        mFragmentList.add(fragment);
+    }
+
+}
